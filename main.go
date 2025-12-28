8baeb64aa3b4cde7955ab5cd157691dc774d4ed7
Harsh <thecoachdefence112@gmail.com>
2025-12-28 14:52:36 +0000

    boilerplater for http server

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	godotenv.Load(".env")
+
+	portstring := os.Getenv("PORT")
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/error", handlerError)
+
+	router.Mount("/v1", v1Router)
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + portstring,
+	}
+	log.Printf("Server starting on PORT %v", portstring)
+	srv.ListenAndServe()
+}
